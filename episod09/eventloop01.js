bbbbc1b54d0c6cf374f4f105688227260fa45b46
Prince Shukla <prinshusdework@gmail.com>
2026-07-05 12:37:44 +0530

    Enhanced event loop demonstration with Promise and setTimeout; added nextTick and improved callback handling.

--- episod09/eventloop01.js
+++ episod09/eventloop01.js
@@ -4,11 +4,37 @@ const a=100;
 
 setImmediate(()=>console.log("setImmediate"))
 
+Promise.resolve("Primise").then(console.log)
+
 fs.readFile("./file.txt","utf8",()=>{
     console.log("File reading CB")
 })
 
 setTimeout(()=>console.log("Timmer Expired"),0)
+
+const myPromise = new Promise((resolve,reject)=>{
+         setTimeout(()=>{
+               if(1){
+                  resolve("Promise resolved")
+               }else{
+                   reject("Promise rejected")
+               }
+         },0)
+})
+
+myPromise.then(fullfilledA,fullfilledB)
+
+function fullfilledA(value){
+    console.log("Resolved: ",value)
+}
+
+function fullfilledB(value){
+    console.log("Rejected: ",value)
+}
+
+process.nextTick(()=>console.log("Next.tick"))
+
+
 
 function printA(){
     console.log("a: ",a)
@@ -17,4 +43,5 @@ function printA(){
 printA()
 
 console.log("Last line of the file")
+
 
